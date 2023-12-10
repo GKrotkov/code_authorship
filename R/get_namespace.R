57688506ccc37bd8e2ad032a1921f62a6f82b18e
gkrotkov <gabriel.gk.mail@gmail.com>
2023-12-10 16:28:57 -0500

    namespaces scripting

---
+++ R/get_namespace.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+library(roxygen2)
+pkgs <- c("utils", "stats", "ggplot2", "dplyr", "tibble", "readr", "roxygen2")
+namespaces <- c()
+
+get_namespace <- function(ns){
+    return(ls(name = asNamespace(ns), all.names = TRUE))
+}
+
+for (i in 1:length(pkgs)){
+    namespaces <- c(namespaces, get_namespace(pkgs[i]))
+}
+
+save(namespaces, file = "r_namespaces.rda")
