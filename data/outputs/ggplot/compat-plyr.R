51d9a8307362fdf9bc2345998c8ccd2ab6bc2b5d
gkrotkov <gabriel.gk.mail@gmail.com>
2023-12-10 19:00:53 -0500

    adding ggplot source code

---
+++ data/outputs/ggplot/compat-plyr.R
@@ -0,0 +1,314 @@
+#' Adds missing elements to a vector from a default vector
+#'
+#' This function appends a given named vector or list with additional elements
+#' from a default vector, only adding those that does not already exist in the
+#' first.
+#'
+#' @param x,y Named vectors or lists
+#'
+#' @return `x` with missing values from `y` appended
+#'
+#' @keywords internal
+#' @noRd
+#'
+defaults <- function(x, y) c(x, y[setdiff(names(y), names(x))])
+# Remove rownames from data frames and matrices
+unrowname <- function(x) {
+  if (is.data.frame(x)) {
+    attr(x, "row.names") <- .set_row_names(.row_names_info(x, 2L))
+  } else if (is.matrix(x)) {
+    dimnames(x)[1] <- list(NULL)
+  } else {
+    cli::cli_abort("Can only remove rownames from {.cls data.frame} and {.cls matrix} objects.")
+  }
+  x
+}
+#' Rename elements in a list, data.frame or vector
+#'
+#' This is akin to `dplyr::rename` and `plyr::rename`. It renames elements given
+#' as names in the `replace` vector to the values in the `replace` vector
+#' without touching elements not referenced.
+#'
+#' @param x A data.frame or a named vector or list
+#' @param replace A named character vector. The names identifies the elements in
+#' `x` that should be renamed and the values gives the new names.
+#'
+#' @return `x`, with new names according to `replace`
+#'
+#' @keywords internal
+#' @noRd
+#'
+rename <- function(x, replace) {
+  current_names <- names(x)
+  old_names <- names(replace)
+  missing_names <- setdiff(old_names, current_names)
+  if (length(missing_names) > 0) {
+    replace <- replace[!old_names %in% missing_names]
+    old_names <- names(replace)
+  }
+  names(x)[match(old_names, current_names)] <- as.vector(replace)
+  x
+}
+# Adapted from plyr:::id_vars
+# Create a unique id for elements in a single vector
+id_var <- function(x, drop = FALSE) {
+  if (length(x) == 0) {
+    id <- integer()
+    n = 0L
+  } else if (!is.null(attr(x, "n")) && !drop) {
+    return(x)
+  } else if (is.factor(x) && !drop) {
+    x <- addNA(x, ifany = TRUE)
+    id <- as.integer(x)
+    n <- length(levels(x))
+  } else {
+    levels <- sort(unique0(x), na.last = TRUE)
+    id <- match(x, levels)
+    n <- max(id)
+  }
+  attr(id, "n") <- n
+  id
+}
+#' Create an unique integer id for each unique row in a data.frame
+#'
+#' Properties:
+#' - `order(id)` is equivalent to `do.call(order, df)`
+#' - rows containing the same data have the same value
+#' - if `drop = FALSE` then room for all possibilities
+#'
+#' @param .variables list of variables
+#' @param drop Should unused factor levels be dropped?
+#'
+#' @return An integer vector with attribute `n` giving the total number of
+#' possible unique rows
+#'
+#' @keywords internal
+#' @noRd
+#'
+id <- function(.variables, drop = FALSE) {
+  nrows <- NULL
+  if (is.data.frame(.variables)) {
+    nrows <- nrow(.variables)
+    .variables <- unclass(.variables)
+  }
+  lengths <- lengths(.variables)
+  .variables <- .variables[lengths != 0]
+  if (length(.variables) == 0) {
+    n <- nrows %||% 0L
+    id <- seq_len(n)
+    attr(id, "n") <- n
+    return(id)
+  }
+  if (length(.variables) == 1) {
+    return(id_var(.variables[[1]], drop = drop))
+  }
+  ids <- rev(lapply(.variables, id_var, drop = drop))
+  p <- length(ids)
+  ndistinct <- vapply(ids, attr, "n", FUN.VALUE = numeric(1), USE.NAMES = FALSE)
+  n <- prod(ndistinct)
+  if (n > 2^31) {
+    char_id <- inject(paste(!!!ids, sep = "\r"))
+    res <- match(char_id, unique0(char_id))
+  }
+  else {
+    combs <- c(1, cumprod(ndistinct[-p]))
+    mat <- inject(cbind(!!!ids))
+    res <- c((mat - 1L) %*% combs + 1L)
+  }
+  if (drop) {
+    id_var(res, drop = TRUE)
+  }
+  else {
+    res <- as.integer(res)
+    attr(res, "n") <- n
+    res
+  }
+}
+#' Count number of occurrences for each unique combination of variables
+#'
+#' Each unique combination of the variables in `df` given by `vars` will be
+#' identified and their occurrences counted. If `wt_var` is given the counts will
+#' be weighted by the values in this column.
+#'
+#' @param df A data.frame
+#' @param vars A vector of column names. If `NULL` all columns in `df` will be
+#' used
+#' @param wt_var The name of a column to use as weight
+#'
+#' @return A data.frame with the unique combinations counted along with a `n`
+#' column giving the counts
+#'
+#' @keywords internal
+#' @noRd
+#'
+count <- function(df, vars = NULL, wt_var = NULL) {
+  df2 <- if (is.null(vars)) df else df[vars]
+  id <- id(df2, drop = TRUE)
+  u_id <- !duplicated(id)
+  labels <- df2[u_id, , drop = FALSE]
+  labels <- labels[order(id[u_id]), , drop = FALSE]
+  if (is.null(wt_var)) {
+    freq <- tabulate(id, attr(id, "n"))
+  } else {
+    wt <- .subset2(df, wt_var)
+    freq <- vapply(split(wt, id), sum, numeric(1))
+  }
+  data_frame0(labels, n = freq)
+}
+# Adapted from plyr::join.keys
+# Create a shared unique id across two data frames such that common variable
+# combinations in the two data frames gets the same id
+join_keys <- function(x, y, by) {
+  joint <- vec_rbind0(x[by], y[by])
+  keys <- id(joint, drop = TRUE)
+  n_x <- nrow(x)
+  n_y <- nrow(y)
+  list(x = keys[seq_len(n_x)], y = keys[n_x + seq_len(n_y)],
+       n = attr(keys, "n"))
+}
+#' Replace specified values with new values, in a factor or character vector
+#'
+#' An easy to use substitution of elements in a string-like vector (character or
+#' factor). If `x` is a character vector the matching elements will be replaced
+#' directly and if `x` is a factor the matching levels will be replaced
+#'
+#' @param x A character or factor vector
+#' @param replace A named character vector with the names corresponding to the
+#' elements to replace and the values giving the replacement.
+#'
+#' @return A vector of the same class as `x` with the given values replaced
+#'
+#' @keywords internal
+#' @noRd
+#'
+revalue <- function(x, replace) {
+  if (is.character(x)) {
+    replace <- replace[names(replace) %in% x]
+    if (length(replace) == 0) return(x)
+    x[match(names(replace), x)] <- replace
+  } else if (is.factor(x)) {
+    lev <- levels(x)
+    replace <- replace[names(replace) %in% lev]
+    if (length(replace) == 0) return(x)
+    lev[match(names(replace), lev)] <- replace
+    levels(x) <- lev
+  } else if (!is.null(x)) {
+    stop_input_type(x, "a factor or character vector")
+  }
+  x
+}
+# Iterate through a formula and return a quoted version
+simplify_formula <- function(x) {
+  if (length(x) == 2 && x[[1]] == as.name("~")) {
+    return(simplify(x[[2]]))
+  }
+  if (length(x) < 3)
+    return(list(x))
+  op <- x[[1]]
+  a <- x[[2]]
+  b <- x[[3]]
+  if (op == as.name("+") || op == as.name("*") || op ==
+      as.name("~")) {
+    c(simplify(a), simplify(b))
+  }
+  else if (op == as.name("-")) {
+    c(simplify(a), bquote(-.(x), list(x = simplify(b))))
+  }
+  else {
+    list(x)
+  }
+}
+#' Create a quoted version of x
+#'
+#' This function captures the special meaning of formulas in the context of
+#' facets in ggplot2, where `+` have special meaning. It works as
+#' `plyr::as.quoted` but only for the special cases of `character`, `call`, and
+#' `formula` input as these are the only situations relevant for ggplot2.
+#'
+#' @param x A formula, string, or call to be quoted
+#' @param env The environment to a attach to the quoted expression.
+#'
+#' @keywords internal
+#' @noRd
+#'
+as.quoted <- function(x, env = parent.frame()) {
+  x <- if (is.character(x)) {
+    lapply(x, function(x) parse(text = x)[[1]])
+  } else if (is.formula(x)) {
+    simplify_formula(x)
+  } else if (is.call(x)) {
+    as.list(x)[-1]
+  } else {
+    cli::cli_abort("Must be a character vector, call, or formula.")
+  }
+  attributes(x) <- list(env = env, class = 'quoted')
+  x
+}
+# round a number to a given precision
+round_any <- function(x, accuracy, f = round) {
+  check_numeric(x)
+  f(x/accuracy) * accuracy
+}
+
+#' Apply function to unique subsets of a data.frame
+#'
+#' This function is akin to `plyr::ddply`. It takes a single data.frame,
+#' splits it by the unique combinations of the columns given in `by`, apply a
+#' function to each split, and then reassembles the results into a sigle
+#' data.frame again.
+#'
+#' @param df A data.frame
+#' @param by A character vector of column names to split by
+#' @param fun A function to apply to each split
+#' @param ... Further arguments to `fun`
+#' @param drop Should unused factor levels in the columns given in `by` be
+#' dropped.
+#'
+#' @return A data.frame if the result of `fun` does not include the columns
+#' given in `by` these will be prepended to the result.
+#'
+#' @keywords internal
+#' @noRd
+dapply <- function(df, by, fun, ..., drop = TRUE) {
+  grouping_cols <- .subset(df, by)
+  fallback_order <- unique0(c(by, names(df)))
+  apply_fun <- function(x) {
+    res <- fun(x, ...)
+    if (is.null(res)) return(res)
+    if (length(res) == 0) return(data_frame0())
+    vars <- lapply(setNames(by, by), function(col) .subset2(x, col)[1])
+    if (is.matrix(res)) res <- split_matrix(res)
+    if (is.null(names(res))) names(res) <- paste0("V", seq_along(res))
+    if (all(by %in% names(res))) return(data_frame0(!!!unclass(res)))
+    res <- modify_list(unclass(vars), unclass(res))
+    res <- res[intersect(c(fallback_order, names(res)), names(res))]
+    data_frame0(!!!res)
+  }
+
+  # Shortcut when only one group
+  if (all(vapply(grouping_cols, single_value, logical(1)))) {
+    return(apply_fun(df))
+  }
+
+  ids <- id(grouping_cols, drop = drop)
+  group_rows <- split_with_index(seq_len(nrow(df)), ids)
+  result <- lapply(seq_along(group_rows), function(i) {
+    cur_data <- df_rows(df, group_rows[[i]])
+    apply_fun(cur_data)
+  })
+  vec_rbind0(!!!result)
+}
+
+single_value <- function(x, ...) {
+  UseMethod("single_value")
+}
+#' @export
+single_value.default <- function(x, ...) {
+  # This is set by id() used in creating the grouping var
+  identical(attr(x, "n"), 1L)
+}
+#' @export
+single_value.factor <- function(x, ...) {
+  # Panels are encoded as factor numbers and can never be missing (NA)
+  identical(levels(x), "1")
+}
---
+++ data/outputs/ggplot/layout.R
@@ -0,0 +1,324 @@
+# The job of `Layout` is to coordinate:
+# * The coordinate system
+# * The faceting specification
+# * The individual position scales for each panel
+#
+# This includes managing the parameters for the facet and the coord
+# so that we don't modify the ggproto object in place.
+
+create_layout <- function(facet = FacetNull, coord = CoordCartesian) {
+  ggproto(NULL, Layout, facet = facet, coord = coord)
+}
+#' @rdname ggplot2-ggproto
+#' @format NULL
+#' @usage NULL
+#' @export
+Layout <- ggproto("Layout", NULL,
+  # The coordinate system and its parameters
+  coord = NULL,
+  coord_params = list(),
+
+  # The faceting specification and its parameters
+  facet = NULL,
+  facet_params = list(),
+
+  # A data frame giving the layout of the data into panels
+  layout = NULL,
+
+  # Per panel scales and params
+  panel_scales_x = NULL,
+  panel_scales_y = NULL,
+  panel_params = NULL,
+
+  setup = function(self, data, plot_data = data_frame0(), plot_env = emptyenv()) {
+    data <- c(list(plot_data), data)
+
+    # Setup facets
+    self$facet_params <- self$facet$setup_params(data, self$facet$params)
+    self$facet_params$plot_env <- plot_env
+    data <- self$facet$setup_data(data, self$facet_params)
+
+    # Setup coords
+    self$coord_params <- self$coord$setup_params(data)
+    data <- self$coord$setup_data(data, self$coord_params)
+
+    # Generate panel layout
+    self$layout <- self$facet$compute_layout(data, self$facet_params)
+    self$layout <- self$coord$setup_layout(self$layout, self$coord_params)
+    check_layout(self$layout)
+
+    # Add panel coordinates to the data for each layer
+    lapply(data[-1], self$facet$map_data,
+      layout = self$layout,
+      params = self$facet_params
+    )
+  },
+
+  # Assemble the facet fg & bg, the coord fg & bg, and the layers
+  # Returns a gtable
+  render = function(self, panels, data, theme, labels) {
+    facet_bg <- self$facet$draw_back(data,
+      self$layout,
+      self$panel_scales_x,
+      self$panel_scales_y,
+      theme,
+      self$facet_params
+    )
+    facet_fg <- self$facet$draw_front(
+      data,
+      self$layout,
+      self$panel_scales_x,
+      self$panel_scales_y,
+      theme,
+      self$facet_params
+    )
+
+    # Draw individual panels, then assemble into gtable
+    panels <- lapply(seq_along(panels[[1]]), function(i) {
+      panel <- lapply(panels, `[[`, i)
+      panel <- c(facet_bg[i], panel, facet_fg[i])
+
+      coord_fg <- self$coord$render_fg(self$panel_params[[i]], theme)
+      coord_bg <- self$coord$render_bg(self$panel_params[[i]], theme)
+      if (isTRUE(theme$panel.ontop)) {
+        panel <- c(panel, list(coord_bg), list(coord_fg))
+      } else {
+        panel <- c(list(coord_bg), panel, list(coord_fg))
+      }
+
+      ggname(
+        paste("panel", i, sep = "-"),
+        gTree(children = inject(gList(!!!panel)))
+      )
+    })
+    plot_table <- self$facet$draw_panels(
+      panels,
+      self$layout,
+      self$panel_scales_x,
+      self$panel_scales_y,
+      self$panel_params,
+      self$coord,
+      data,
+      theme,
+      self$facet_params
+    )
+
+    # Draw individual labels, then add to gtable
+    labels <- self$coord$labels(
+      list(
+        x = self$resolve_label(self$panel_scales_x[[1]], labels),
+        y = self$resolve_label(self$panel_scales_y[[1]], labels)
+      ),
+      self$panel_params[[1]]
+    )
+    labels <- self$render_labels(labels, theme)
+    self$facet$draw_labels(
+      plot_table,
+      self$layout,
+      self$panel_scales_x,
+      self$panel_scales_y,
+      self$panel_params,
+      self$coord,
+      data,
+      theme,
+      labels,
+      self$params
+    )
+  },
+
+  train_position = function(self, data, x_scale, y_scale) {
+    # Initialise scales if needed, and possible.
+    layout <- self$layout
+    if (is.null(self$panel_scales_x)) {
+      self$panel_scales_x <- self$facet$init_scales(layout, x_scale = x_scale,
+        params = self$facet_params)$x
+    }
+    if (is.null(self$panel_scales_y)) {
+      self$panel_scales_y <- self$facet$init_scales(layout, y_scale = y_scale,
+        params = self$facet_params)$y
+    }
+
+    self$facet$train_scales(
+      self$panel_scales_x,
+      self$panel_scales_y,
+      layout,
+      data,
+      self$facet_params
+    )
+  },
+
+  map_position = function(self, data) {
+    layout <- self$layout
+
+    lapply(data, function(layer_data) {
+      match_id <- NULL
+
+      # Loop through each variable, mapping across each scale, then joining
+      # back together
+      x_vars <- intersect(self$panel_scales_x[[1]]$aesthetics, names(layer_data))
+      if (length(x_vars) > 0) {
+        match_id <- match(layer_data$PANEL, layout$PANEL)
+        names(x_vars) <- x_vars
+        SCALE_X <- layout$SCALE_X[match_id]
+        new_x <- scale_apply(layer_data, x_vars, "map", SCALE_X, self$panel_scales_x)
+        layer_data[, x_vars] <- new_x
+      }
+
+      y_vars <- intersect(self$panel_scales_y[[1]]$aesthetics, names(layer_data))
+      if (length(y_vars) > 0) {
+        if (is.null(match_id)) {
+          match_id <- match(layer_data$PANEL, layout$PANEL)
+        }
+        names(y_vars) <- y_vars
+        SCALE_Y <- layout$SCALE_Y[match_id]
+        new_y <- scale_apply(layer_data, y_vars, "map", SCALE_Y, self$panel_scales_y)
+        layer_data[, y_vars] <- new_y
+      }
+
+      layer_data
+    })
+  },
+
+  reset_scales = function(self) {
+    if (!self$facet$shrink) return()
+    lapply(self$panel_scales_x, function(s) s$reset())
+    lapply(self$panel_scales_y, function(s) s$reset())
+    invisible()
+  },
+
+  finish_data = function(self, data) {
+    lapply(data, self$facet$finish_data,
+      layout = self$layout,
+      x_scales = self$panel_scales_x,
+      y_scales = self$panel_scales_y,
+      params = self$facet_params
+    )
+  },
+
+  get_scales = function(self, i) {
+    this_panel <- self$layout[self$layout$PANEL == i, ]
+
+    list(
+      x = self$panel_scales_x[[this_panel$SCALE_X]],
+      y = self$panel_scales_y[[this_panel$SCALE_Y]]
+    )
+  },
+
+  setup_panel_params = function(self) {
+    # Fudge for CoordFlip and CoordPolar - in place modification of
+    # scales is not elegant, but it is pragmatic
+    self$coord$modify_scales(self$panel_scales_x, self$panel_scales_y)
+
+    scales_x <- self$panel_scales_x[self$layout$SCALE_X]
+    scales_y <- self$panel_scales_y[self$layout$SCALE_Y]
+
+    setup_panel_params <- function(scale_x, scale_y) {
+      self$coord$setup_panel_params(scale_x, scale_y, params = self$coord_params)
+    }
+    self$panel_params <- Map(setup_panel_params, scales_x, scales_y)
+
+    invisible()
+  },
+
+  setup_panel_guides = function(self, guides, layers) {
+    self$panel_params <- lapply(
+      self$panel_params,
+      self$coord$setup_panel_guides,
+      guides,
+      self$coord_params
+    )
+
+    self$panel_params <- lapply(
+      self$panel_params,
+      self$coord$train_panel_guides,
+      layers,
+      self$coord_params
+    )
+
+    invisible()
+  },
+
+  resolve_label = function(self, scale, labels) {
+    # General order is: guide title > scale name > labels
+    aes       <- scale$aesthetics[[1]]
+    primary   <- scale$name %|W|% labels[[aes]]
+    secondary <- if (is.null(scale$secondary.axis)) {
+      waiver()
+    } else {
+      scale$sec_name()
+    } %|W|% labels[[paste0("sec.", aes)]]
+    if (is.derived(secondary)) secondary <- primary
+    order <- scale$axis_order()
+
+    if (!is.null(self$panel_params[[1]]$guides)) {
+      if ((scale$position) %in% c("left", "right")) {
+        guides <- c("y", "y.sec")
+      } else {
+        guides <- c("x", "x.sec")
+      }
+      params    <- self$panel_params[[1]]$guides$get_params(guides)
+      if (!is.null(params)) {
+        primary   <- params[[1]]$title %|W|% primary
+        secondary <- params[[2]]$title %|W|% secondary
+        position  <- params[[1]]$position %||% scale$position
+        if (position != scale$position) {
+          order <- rev(order)
+        }
+      }
+    }
+    primary   <- scale$make_title(primary)
+    secondary <- scale$make_sec_title(secondary)
+    list(primary = primary, secondary = secondary)[order]
+  },
+
+  render_labels = function(self, labels, theme) {
+    label_grobs <- lapply(names(labels), function(label) {
+      lapply(c(1, 2), function(i) {
+        modify <- if (i == 1) {
+          switch(label, x = ".top", y = ".left")
+        } else {
+          switch(label, x = ".bottom", y = ".right")
+        }
+        if (is.null(labels[[label]][[i]]) || is.waive(labels[[label]][[i]]))
+          return(zeroGrob())
+
+        element_render(
+          theme = theme,
+          element = paste0("axis.title.", label, modify),
+          label = labels[[label]][[i]],
+          margin_x = label == "y",
+          margin_y = label == "x"
+        )
+      })
+    })
+    names(label_grobs) <- names(labels)
+    label_grobs
+  }
+)
+
+
+# Helpers -----------------------------------------------------------------
+
+# Function for applying scale method to multiple variables in a given
+# data set.  Implement in such a way to minimize copying and hence maximise
+# speed
+scale_apply <- function(data, vars, method, scale_id, scales) {
+  if (length(vars) == 0) return()
+  if (nrow(data) == 0) return()
+
+  if (anyNA(scale_id)) {
+    cli::cli_abort("{.arg scale_id} must not contain any {.val NA}.")
+  }
+
+  scale_index <- split_with_index(seq_along(scale_id), scale_id, length(scales))
+
+  lapply(vars, function(var) {
+    pieces <- lapply(seq_along(scales), function(i) {
+      scales[[i]][[method]](data[[var]][scale_index[[i]]])
+    })
+    # Remove empty vectors to avoid coercion issues with vctrs
+    pieces[lengths(pieces) == 0] <- NULL
+    o <- order(unlist(scale_index))[seq_len(sum(lengths(pieces)))]
+    vec_c(!!!pieces)[o]
+  })
+}
