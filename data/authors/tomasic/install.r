ea6f5a3fd191fc257ffc11d24cde7db42d95cea2
gkrotkov <gabriel.gk.mail@gmail.com>
2023-12-11 10:19:49 -0500

    cleaning up one author

--- data/authors/tomasic/install.r
+++
@@ -1,5 +0,0 @@
-install.packages("FNN") # knn
-install.packages("MASS") # rlm
-install.packages("e1071") # kurtosis
-install.packages("randomForest") # random forests
-install.paxkages("neuralnet") # mlp
