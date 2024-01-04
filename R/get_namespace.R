330cfdf5500429a688b9815e70f6343eb5aaecd0
gkrotkov <gabriel.gk.mail@gmail.com>
2024-01-03 22:36:24 -0600

    restructuring namespaces code

--- R/get_namespace.R
+++ R/get_namespace.R
@@ -2,16 +2,20 @@ library(tidyverse)
 library(roxygen2)
 library(devtools)
 
-pkgs <- c("base", "utils", "stats", "ggplot2", "dplyr", "tibble", "readr", 
-          "roxygen2", "devtools")
-namespaces <- c()
-
 get_namespace <- function(ns){
     return(ls(name = asNamespace(ns), all.names = TRUE))
 }
 
-for (i in 1:length(pkgs)){
-    namespaces <- c(namespaces, get_namespace(pkgs[i]))
-}
 
-save(namespaces, file = "r_namespaces.rda")
+get_namespaces <- function(pkgs = c("base", "utils", "stats", "ggplot2", 
+                                    "dplyr", "tibble", "readr", "roxygen2", 
+                                    "devtools"), 
+                           tofile = "data/r_namespaces.rda"){
+    namespaces <- c()
+    
+    for (i in 1:length(pkgs)){
+        namespaces <- c(namespaces, get_namespace(pkgs[i]))
+    }
+    
+    save(namespaces, file = tofile)
+}
